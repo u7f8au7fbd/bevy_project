9ece140057ee8ba8c85cced9730c644755fa4d38
u7f8au7fbd <u7f8au7fbd@outlook.com>
2024-04-24 21:54:23 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -36,22 +36,101 @@ fn main() {
             InfiniteGridPlugin,                 //グリッドを表示
         ))
         //以上は固定
-        .add_systems(Startup, example) 
+        .add_systems(Startup, summon)
+        .add_systems(Update, (move_player, move_camera))
         .run();
 }
 
-//例
-fn example(mut commands: Commands) {
-    //グリッドを生成
+#[derive(Component)]
+pub struct Player;
+
+pub fn summon(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
     commands.spawn(InfiniteGridBundle::default());
-    //ライトを生成
+
     commands.spawn(DirectionalLightBundle {
         transform: Transform::from_translation(Vec3::ONE).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
-    //カメラを生成
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(16.0, 4.0, 16.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
+
+    // Player
+    let default_fov = 70.53_f32.to_radians(); //デフォルトの垂直視野角70.53度(水平視野角は103度)
+    commands
+        .spawn((
+            InheritedVisibility::default(),//描画オブジェクト
+            Player,
+            Camera3dBundle {
+                transform: Transform::from_xyz(0.0, 4.0, 40.0),
+                projection: Projection::Perspective(PerspectiveProjection {
+                    fov: default_fov,
+                    ..default()
+                }),
+                ..default()
+            },
+            FogSettings {
+                color: Color::rgba(0., 0., 0., 1.0),
+                falloff: FogFalloff::Linear {
+                    start: 20.0,
+                    end: 48.0,
+                },
+                ..default()
+            },
+            SpatialListener {
+                left_ear_offset: Vec3::new(0.1, 0.0, 0.0),
+                right_ear_offset: Vec3::new(-0.1, 0.0, 0.0),
+            },
+        ))
+        .with_children(|interacter| {
+            interacter.spawn((PbrBundle {
+                mesh: meshes.add(Cuboid::new(0.05, 0.05, 0.05)),
+                material: materials.add(Color::rgb(1., 1., 1.)),
+                transform: Transform {
+                    translation: Vec3::new(0.0, 0.0, -4.0),
+                    ..default()
+                },
+                ..default()
+            },));
+        });
+}
+
+fn move_player(
+    mut query: Query<&mut Transform, With<Player>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+) {
+    let speed = 12.;
+    for mut transform in query.iter_mut() {
+        let rotation = transform.rotation;
+        if keyboard_input.pressed(KeyCode::KeyW) {
+            transform.translation -= rotation * Vec3::Z * speed * time.delta_seconds();
+        }
+        if keyboard_input.pressed(KeyCode::KeyS) {
+            transform.translation += rotation * Vec3::Z * speed * time.delta_seconds();
+        }
+        if keyboard_input.pressed(KeyCode::KeyA) {
+            transform.translation -= rotation * Vec3::X * speed * time.delta_seconds();
+        }
+        if keyboard_input.pressed(KeyCode::KeyD) {
+            transform.translation += rotation * Vec3::X * speed * time.delta_seconds();
+        }
+    }
+}
+
+fn move_camera(
+    mut query: Query<&mut Transform, With<Player>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+) {
+    let sensitivity = 2.;
+    for mut transform in query.iter_mut() {
+        if keyboard_input.pressed(KeyCode::ArrowRight) {
+            transform.rotation *= Quat::from_rotation_y(-sensitivity * time.delta_seconds());
+        }
+        if keyboard_input.pressed(KeyCode::ArrowLeft) {
+            transform.rotation *= Quat::from_rotation_y(sensitivity * time.delta_seconds());
+        }
+    }
 }
